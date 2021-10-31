dc75b3deb146d9711f7538ba44908d5d2a04ffa8
mital2021 <87838938+mital2021@users.noreply.github.com>
2021-10-31 15:08:35 -0400

    generated index.html

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
- const fs = require('fs');
- const inquirer = require('inquirer');
- const Engineer = require('./lib/Engineer');
- const Intern = require('./lib/Intern');
- const Manager = require('./lib/Manager');
- const generatePage = require('./src/page-template');
- const { writeFile, copyFile } = require('./src/generate-site');
+const fs = require('fs');
+const inquirer = require('inquirer');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+const generatePage = require('./src/page-template.js');
+const { writeFile, copyFile } = require('./src/generate-site.js');
 
